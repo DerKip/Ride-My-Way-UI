f8e0a269e4cbbd5761b097ebf992945b0a09b507
Derrick Kipchumba <dkip64@gmail.com>
2018-07-29 22:32:05 +0300

    [finishes #159294220 ft] add log in function

--- js/auth.js
+++ js/auth.js
@@ -64,7 +64,7 @@ function signupWithCar(event){
     let success = document.getElementById('message');
     let status = '';
     
-    fetch('https://ride-my-way-2.herokuapp.com/api/v2/auth/signup', {
+    fetch('https://ride-my-way-2.herokuapp.com/api/v2/auth/signup', { 
         method: 'POST',
         headers: {
             'Content-type':'application/json'
@@ -93,6 +93,52 @@ function signupWithCar(event){
             success.style.display= 'block';
             document.getElementById('message').innerHTML = data['message'];
         }
+    })
+    .catch((err)=>console.log(err))
+}
+
+
+//Login
+document.getElementById('login').addEventListener('submit', loginUser);
+
+function loginUser(event){   
+    event.preventDefault();
+
+    let username = document.getElementById('visitor').value;
+    let password = document.getElementById('password_login').value;
+   
+    //declare error,messages and status variables to be used (user interaction)
+    let error = document.getElementById('error');
+    let success = document.getElementById('message');
+    let status = '';
+    
+    fetch('https://ride-my-way-2.herokuapp.com/api/v2/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify(
+            {
+                username:username,
+                password:password
+        })
+    }).then((res) => {
+        status = res.status;
+        return res.json();
+    })  
+    .then((data) => {
+        if (status >= 400){
+            error.style.display='block';
+            document.getElementById('error').innerHTML = data["error"];     
+        }
+        if (status == 200 ){
+            success.style.display= 'block';
+            document.getElementById('message').innerHTML = data['message'];
+            //store access_token to web local storage
+            window.localStorage.setItem('token', data.token);
+            //redirect user to  all ride offers available
+            // window.location = 'all_ride_offers.html'; 
+        }    
     })
     .catch((err)=>console.log(err))
 }
